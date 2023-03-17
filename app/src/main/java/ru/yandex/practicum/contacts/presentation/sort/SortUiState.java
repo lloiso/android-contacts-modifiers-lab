e96379d8c6d17aa02b0be43b4f3edf3531bcae8c
dmitrii <llosio@mail.ru>
2023-03-17 10:27:36 +0300

    init commit

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -4,8 +4,8 @@ package ru.yandex.practicum.contacts.presentation.sort;
 public class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    private boolean isApplyEnable = false;
+    private String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
 }
